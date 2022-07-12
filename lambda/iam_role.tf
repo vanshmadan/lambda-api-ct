f9f46b90a8157b260a90abc4f589608a12017fbe
Vansh Madan <31353777+vanshmadan@users.noreply.github.com>
2022-07-13 03:00:00 +0530

    Update iam_role.tf

--- lambda/iam_role.tf
+++ lambda/iam_role.tf
@@ -1,4 +1,6 @@
-data "aws_iam_policy_document" "assume_ec2_role_policy" {
+################### Lambda role ###################
+
+data "aws_iam_policy_document" "assume_lambda_role_policy" {
   statement {
     sid = "assumePolicy"
     actions = [
@@ -14,11 +16,12 @@ data "aws_iam_policy_document" "assume_ec2_role_policy" {
 
 module "s3_lambda_us_east_1" {
   source             = "../modules/iam_roles"
-  assume_role_policy = data.aws_iam_policy_document.assume_ec2_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role_policy.json
   name               = "lambda-${var.env}"
   tags               = merge(local.global_tags, { "Creator" : "vanshkumar@lambdatest.com" })
 }
 
+  ################### Api gateway lambda invoke role ###################
 
 data "aws_iam_policy_document" "assume_lambda_role_policy" {
   statement {
